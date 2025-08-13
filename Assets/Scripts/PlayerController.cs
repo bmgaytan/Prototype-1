3ea381159c0d2d2b782814788d245ee69fb36f73
bmgaytan <bmgaytan3114@gmail.com>
2025-08-13 18:42:16 -0400

    updated PlayerController script
    made the vehicle move forward

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,6 +11,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // We'll move the vehicle forward
+        transform.Translate(Vector3.forward);
     }
 }
